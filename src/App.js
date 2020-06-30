1058347e693cf93d6e1d76b415ebad226779e7c7
Emmanuel Adedeji <emmans4destiny@gmail.com>
2020-06-30 17:27:11 +0300

    console log

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ class App extends React.Component {
     })
    }
   render() {
+    console.log(this.state.comments)
     return (
       <div className="App">
         <header className="App-header">
